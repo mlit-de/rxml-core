1315c2d319fa814d7dadc49ff9921dd2d6044d6b
Markus Lauer <markus.lauer-2015@mlit.de>
2016-02-26 08:52:11 +0100

    Added initial mode

---
+++ src/main/java/de/mlit/rxml/xslt/XsltResource3.java
@@ -0,0 +1,154 @@
+package de.mlit.rxml.xslt;
+
+
+import de.mlit.rxml.api.SaxResource;
+import de.mlit.rxml.api.helper.AbstractResource;
+import de.mlit.rxml.api.helper.ResourceAdapter;
+import de.mlit.rxml.resolver.RxmlInputSource;
+import de.mlit.rxml.resolver.RxmlResolver;
+import de.mlit.rxml.resolver.RxmlSAXSource;
+import net.sf.saxon.Configuration;
+import net.sf.saxon.jaxp.TemplatesImpl;
+import net.sf.saxon.jaxp.TransformerImpl;
+import net.sf.saxon.lib.FeatureKeys;
+import net.sf.saxon.s9api.*;
+import net.sf.saxon.trans.XPathException;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.ext.LexicalHandler;
+
+import javax.xml.transform.ErrorListener;
+import javax.xml.transform.Templates;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by mlauer on 14/11/14.
+ */
+public class XsltResource3 extends AbstractResource implements SaxResource {
+
+
+    protected SaxResource stylesheet;
+    protected SaxResource document;
+
+    protected Map<String, Object> map;
+    protected RxmlResolver resolver = this.resolver;
+    protected String mode;
+
+    //protected static SAXTransformerFactory factory = (SAXTransformerFactory)TransformerFactory.newInstance();
+
+    protected SAXTransformerFactory factory;
+    protected Processor processor;
+
+
+    public XsltResource3(SaxResource stylesheet, SaxResource document,
+                         Map<String, Object> map, RxmlResolver resolver, List<AbstractSaxonExtension> extensions, String mode) {
+        this.stylesheet = stylesheet;
+        this.document = document;
+        this.map = map;
+        this.mode = mode;
+
+        Configuration configuration = getConfiguration(extensions);
+        if (resolver != null) {
+            configuration.setURIResolver(resolver);
+        }
+        this.processor = new Processor(configuration);
+
+
+    }
+
+
+    protected Configuration getConfiguration(List<AbstractSaxonExtension> extensions) {
+        Configuration configuration = Configuration.newConfiguration();
+        configuration.setConfigurationProperty(FeatureKeys.XSLT_VERSION, "3.0");
+        configuration.setConfigurationProperty(FeatureKeys.XQUERY_VERSION, "3.0");
+        configuration.setConfigurationProperty(FeatureKeys.TRACE_EXTERNAL_FUNCTIONS, false);
+        if (extensions != null) {
+            for (AbstractSaxonExtension ext : extensions) {
+                if (ext != null) {
+                    ext.install(configuration);
+                }
+            }
+        }
+        return configuration;
+    }
+
+
+    @Override
+    public void runOn(ContentHandler ch) throws SAXException, IOException {
+
+        Templates templ = null;
+
+
+        XsltCompiler xsltCompiler = processor.newXsltCompiler();
+        final ErrorListener errorListener = xsltCompiler.getErrorListener();
+        final List<String> errors = new ArrayList<String>();
+
+        ErrorListener errorListener1 = new ErrorListener() {
+            @Override
+            public void warning(TransformerException exception) throws TransformerException {
+                errorListener.warning(exception);
+            }
+
+            @Override
+            public void error(TransformerException exception) throws TransformerException {
+                errorListener.error(exception);
+            }
+
+            @Override
+            public void fatalError(TransformerException exception) throws TransformerException {
+                if (exception instanceof XPathException) {
+                    errors.add(
+                            ((XPathException) exception).getLocationAsString() + " " +
+                                    ((XPathException) exception).getMessage());
+                }
+                errorListener.fatalError(exception);
+            }
+        };
+
+        xsltCompiler.setErrorListener(errorListener1);
+        try {
+            XsltExecutable compile = xsltCompiler.compile(new RxmlSAXSource(this.resolver, new RxmlInputSource(ResourceAdapter.convert(stylesheet))));
+            templ = new TemplatesImpl(compile);
+        } catch (SaxonApiException ex) {
+            throw new RuntimeException(String.join(" ", errors), ex);
+        }
+
+
+        TransformerHandler trh = null;
+        try {
+            TransformerImpl transformer = (TransformerImpl) templ.newTransformer();
+            QName initialMode = transformer.getUnderlyingXsltTransformer().getInitialMode();
+            if(mode!=null && !mode.equals("")) {
+                transformer.getUnderlyingXsltTransformer().setInitialMode(new QName("","",mode));
+            }
+            trh = transformer.newTransformerHandler();
+
+        } catch (TransformerConfigurationException e1) {
+            throw new SAXException(e1);
+        }
+        if (map != null) {
+            for (Map.Entry<String, Object> kv : map.entrySet()) {
+                trh.getTransformer().setParameter(kv.getKey(), kv.getValue());
+            }
+        }
+
+        SAXResult sr = new SAXResult(ch);
+        if (ch instanceof LexicalHandler) {
+            sr.setLexicalHandler((LexicalHandler) ch);
+        }
+        trh.setResult(sr);
+        document.runOn(trh);
+
+    }
+}
+
+
+
