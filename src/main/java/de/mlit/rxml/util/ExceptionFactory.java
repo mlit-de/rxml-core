d509531130ee712784f14a5f137c56f36acf89a8
Markus Lauer <markus.lauer-2015@mlit.de>
2019-09-15 16:06:31 +0200

    added ExceptionFactory

---
+++ src/main/java/de/mlit/rxml/util/ExceptionFactory.java
@@ -0,0 +1,8 @@
+package de.mlit.rxml.util;
+
+public class ExceptionFactory {
+
+    public static void throwRuntimeException(String message) {
+        throw new RuntimeException(message);
+    }
+}
